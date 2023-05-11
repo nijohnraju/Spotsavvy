b496d5a19bd4b8e81938f280caab296b7f33fc26
Leo Jose <85281241+1eojos3@users.noreply.github.com>
2023-05-11 10:27:52 +0530

    hello msg

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';//hello
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:panigale/screens/home.dart';
