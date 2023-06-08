1272d50c5d87dce4cdc0c57784a3e85716c02471
1eojos3 <leojose2024@cs.ajce.in>
2023-06-08 12:30:27 +0530

    added polylines for the map

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'dart:async';
-import 'package:panigale/custom.dart';
+import 'package:http/http.dart' as http;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -17,6 +18,7 @@ class _SearchPageState extends State<SearchPage> {
   static const CameraPosition initialCameraPosition =
       CameraPosition(target: LatLng(37.15478, -122.78945), zoom: 14.0);
   Set<Marker> markers = {};
+  Set<Polyline> polylines = {};
 
   @override
   void initState() {
@@ -31,16 +33,18 @@ class _SearchPageState extends State<SearchPage> {
         markers.add(
           Marker(
             markerId: const MarkerId('currentLocation'),
-            position: LatLng(position.latitude, position.longitude), // Set marker color to red
+            position: LatLng(position.latitude, position.longitude),
+            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+            onTap: () {
+              setState(() {});
+              fetchDirections(position.latitude, position.longitude,
+                  markers.elementAt(1).position.latitude, markers.elementAt(1).position.longitude);
+            },
           ),
         );
       });
 
-      // Fetch locations from the database
-      FirebaseFirestore.instance
-          .collection('Rentdetails')
-          .get()
-          .then((QuerySnapshot querySnapshot) {
+      FirebaseFirestore.instance.collection('Rentdetails').get().then((QuerySnapshot querySnapshot) {
         querySnapshot.docs.forEach((doc) {
           var location = doc.get('Location');
           var latitude = location['latitude'];
@@ -51,7 +55,11 @@ class _SearchPageState extends State<SearchPage> {
               Marker(
                 markerId: MarkerId(doc.id),
                 position: LatLng(latitude, longitude),
-                icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue)
+                icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+                onTap: () {
+                  setState(() {});
+                  fetchDirections(position.latitude, position.longitude, latitude, longitude);
+                },
               ),
             );
           });
@@ -82,11 +90,76 @@ class _SearchPageState extends State<SearchPage> {
     return await Geolocator.getCurrentPosition();
   }
 
+  Future<void> fetchDirections(
+      double originLat, double originLng, double destinationLat, double destinationLng) async {
+    String apiKey = 'AIzaSyDC6Itbcw16HH1H7-eQymYXU7ejd1WPQeI';
+    String url =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=$originLat,$originLng&destination=$destinationLat,$destinationLng&key=$apiKey';
+
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      if (data['status'] == 'OK') {
+        setState(() {
+          polylines.clear();
+
+          List<LatLng> polylineCoordinates = _decodePolyline(data['routes'][0]['overview_polyline']['points']);
+          Polyline polyline = Polyline(
+            polylineId: const PolylineId('directions'),
+            points: polylineCoordinates,
+            color: Colors.blue,
+            width: 5,
+          );
+          polylines.add(polyline);
+        });
+      } else {
+        print('Error fetching directions: ${data['error_message']}');
+      }
+    } else {
+      print('Error fetching directions. Status code: ${response.statusCode}');
+    }
+  }
+
+  List<LatLng> _decodePolyline(String encodedPolyline) {
+    List<LatLng> polylinePoints = [];
+    int index = 0, len = encodedPolyline.length;
+    int lat = 0, lng = 0;
+
+    while (index < len) {
+      int b, shift = 0, result = 0;
+      do {
+        b = encodedPolyline.codeUnitAt(index++) - 63;
+        result |= (b & 0x1F) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      lat += dlat;
+
+      shift = 0;
+      result = 0;
+      do {
+        b = encodedPolyline.codeUnitAt(index++) - 63;
+        result |= (b & 0x1F) << shift;
+        shift += 5;
+      } while (b >= 0x20);
+      int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+      lng += dlng;
+
+      double latitude = lat / 1E5;
+      double longitude = lng / 1E5;
+      LatLng point = LatLng(latitude, longitude);
+      polylinePoints.add(point);
+    }
+
+    return polylinePoints;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: whitesavvy,
+        backgroundColor: Colors.white,
         title: const Text(
           'Book A Parking Spot',
           style: TextStyle(fontWeight: FontWeight.bold),
@@ -96,6 +169,7 @@ class _SearchPageState extends State<SearchPage> {
       body: GoogleMap(
         initialCameraPosition: initialCameraPosition,
         markers: markers,
+        polylines: polylines,
         zoomControlsEnabled: false,
         mapType: MapType.normal,
         onMapCreated: (GoogleMapController controller) {
@@ -113,7 +187,6 @@ class _SearchPageState extends State<SearchPage> {
           ));
         },
         label: const Text("Current Location"),
-        icon: const Icon(Icons.location_history),
       ),
     );
   }
