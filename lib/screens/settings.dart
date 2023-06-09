9699a54172f2b4d2c1987ad53a2a9f9d93ca4f83
1eojos3 <leojose2024@cs.ajce.in>
2023-06-09 23:00:41 +0530

    added about us to settings

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:panigale/custom.dart';
 
 class SettingsPage extends StatelessWidget {
-  const SettingsPage({super.key});
+  const SettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +28,27 @@ class SettingsPage extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     fontSize: 20),
               ),
+              const SizedBox(height: 65),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(50),
+                  backgroundColor: Colors.black, // Change the color to your preference
+                ),
+                child: Text(
+                  'About Us',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white, // Change the color to your preference
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AboutUsPage()),
+                  );
+                },
+              ),
               const SizedBox(
                 height: 40,
               ),
@@ -42,7 +63,7 @@ class SettingsPage extends StatelessWidget {
                   color: blacksavvy,
                 ),
                 label: Text(
-                  'sign out',
+                  'Sign Out',
                   style: TextStyle(
                       fontSize: 24,
                       color: blacksavvy,
@@ -51,6 +72,26 @@ class SettingsPage extends StatelessWidget {
                 onPressed: () => FirebaseAuth.instance.signOut(),
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AboutUsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('About Us'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(32),
+        child: Center(
+          child: Text(
+            'This is the About Us page.',
+            style: TextStyle(fontSize: 24),
           ),
         ),
       ),
